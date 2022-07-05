1d779e18870385ca36027cf49670c4c1cccf7b48
Anish <anishkakaiya@outlook.com>
2022-07-05 23:07:00 +0100

    require controller and refactor

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,17 +1,9 @@
 const express = require('express')
-const User = require('../models/users')
-
+const {
+  createNewUser
+} = require('../controllers/users')
 const router = express.Router()
 
-router.post('/signup', async (req, res) => {
-  const {name, email, password} = req.body
-
-  try {
-    const user = await User.create({name, email, password})
-    res.status(200).json(user)
-  } catch (error) {
-    res.status(400).json({error: error.message})
-  }
-})
+router.post('/signup', createNewUser)
 
 module.exports = router
