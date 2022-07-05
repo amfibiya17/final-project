a6c0e4cf5b1983508e038e99cf179c75d99844f7
Yaroslava Yates <yaroslava.yates@gmail.com>
2022-07-05 23:05:57 +0100

    first part of febe

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes } from 'react-router-dom';
+// import SomeComponent from './components/SomeComponent';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* <Route path='/' element={<LoginPageDetails />} /> */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
