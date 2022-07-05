bdffff169e222e570ecb21e97a86f8e4703b7256
Anish <anishkakaiya@outlook.com>
2022-07-05 17:17:15 +0100

    add cors

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 require('dotenv').config()
 const express = require('express')
-
+const cors = require('cors')
 
 const app = express()
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.json({message: 'App Test'})
