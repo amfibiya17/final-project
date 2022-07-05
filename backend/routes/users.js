7209811f565a7cd438c045f25f97503043447aa0
Anish <anishkakaiya@outlook.com>
2022-07-05 22:55:43 +0100

    add to post request to post user data

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const User = require('../models/users')
 
 const router = express.Router()
 
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
+  const {name, email, password} = req.body
 
+  try {
+    const user = await User.create({name, email, password})
+    res.status(200).json(user)
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
 })
 
 module.exports = router
