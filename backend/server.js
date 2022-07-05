d5fce784cb1432f9424b844eee6193cfc357a0b9
Anish <anishkakaiya@outlook.com>
2022-07-05 17:27:20 +0100

    required routes and refactor

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,13 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const userRoutes = require('./routes/users')
 
 const app = express()
 
 app.use(cors())
 
-app.get('/', (req, res) => {
-  res.json({message: 'App Test'})
-})
+app.use('/users', userRoutes)
 
 app.listen(process.env.PORT, () => {
   console.log(`Listening on Port ${process.env.PORT}`)
