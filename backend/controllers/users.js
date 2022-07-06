1fb5d48284b281424f716a2c1d67b3bba88ea2c2
Anish <anishkakaiya@outlook.com>
2022-07-06 10:24:42 +0100

    refactor to use js object to hold routes

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,16 +1,17 @@
 const User = require('../models/users')
 
-const createNewUser = async (req, res) => {
-  const {name, email, password} = req.body
+const UserController = {
 
-  try {
-    const user = await User.create({name, email, password})
-    res.status(200).json(user)
-  } catch (error) {
-    res.status(400).json({error: error.message})
+  CreateNewUser: async (req, res) => {
+    const {name, email, password} = req.body
+  
+    try {
+      const user = await User.create({name, email, password})
+      res.status(200).json(user)
+    } catch (error) {
+      res.status(400).json({error: error.message})
+    }
   }
 }
 
-module.exports = {
-  createNewUser
-}
+module.exports = UserController
