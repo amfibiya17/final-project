a3d5b855d717e28a34d8384b1705965077405816
Anish <anishkakaiya@outlook.com>
2022-07-06 10:29:40 +0100

    add login api endpoint

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,5 +3,6 @@ const UserController = require('../controllers/users')
 const router = express.Router()
 
 router.post('/signup', UserController.CreateNewUser)
+router.get('/login', UserController.LoginUser)
 
 module.exports = router
