a1391f0d4aa6956a920e45883be28de80f6b7c1b
Yaroslava Yates <yaroslava.yates@gmail.com>
2022-07-08 18:07:45 +0100

    changes

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -1,33 +1,198 @@
-/* eslint-disable camelcase */
-import React, { useState } from 'react';
-import FullCalendar from '@fullcalendar/react';
+/* eslint-disable class-methods-use-this */
+/* eslint-disable react/no-access-state-in-setstate */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable react/sort-comp */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable max-len */
+/* eslint-disable no-use-before-define */
+/* eslint-disable react/state-in-constructor */
+/* eslint-disable no-restricted-globals */
+/* eslint-disable no-alert */
+// /* eslint-disable camelcase */
+
+// import React, { useState } from 'react';
+// import FullCalendar from '@fullcalendar/react';
+// import dayGridPlugin from '@fullcalendar/daygrid';
+// // import '@fullcalendar/core/main.css';
+// import '@fullcalendar/daygrid/main.css';
+
+// import './custom.css';
+
+// function Calendar() {
+//   const [appointments, setAppointments] = useState([]);
+//   (async () => {
+//     const user_id = '62c6139c9c28b952c0b65659';
+//     const response = await fetch(`http://localhost:4000/appointments/calendar?user_id=${user_id}`);
+//     const records = await response.json();
+
+//     setAppointments(records.map((record) => ({
+//       title: record.name,
+//       date: new Date(record.date),
+//     })));
+//   })();
+
+//   return (
+//     <div>
+//       <FullCalendar
+//         plugins={[dayGridPlugin]}
+//         events={appointments}
+//       />
+//     </div>
+//   );
+// }
+
+// export default Calendar;
+
+import React from 'react';
+import FullCalendar, { formatDate } from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
-// import '@fullcalendar/core/main.css';
-import '@fullcalendar/daygrid/main.css';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import { INITIAL_EVENTS, createEventId } from './event-utils';
+import './main.css';
 
-import './custom.css';
+export default class DemoApp extends React.Component {
+  state = {
+    weekendsVisible: true,
+    currentEvents: [],
+  };
 
-function Calendar() {
-  const [appointments, setAppointments] = useState([]);
-  (async () => {
-    const user_id = '62c6139c9c28b952c0b65659';
-    const response = await fetch(`http://localhost:4000/appointments/calendar?user_id=${user_id}`);
-    const records = await response.json();
+  render() {
+    return (
+      <div className="demo-app">
+        {this.renderSidebar()}
+        <div className="demo-app-main">
+          <FullCalendar
+            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+            headerToolbar={{
+              left: 'prev,next today',
+              center: 'title',
+              right: 'dayGridMonth,timeGridWeek,timeGridDay',
+            }}
+            initialView="dayGridMonth"
+            editable
+            selectable
+            selectMirror
+            dayMaxEvents
+            // events={appointment}
+            weekends={this.state.weekendsVisible}
+            initialEvents={INITIAL_EVENTS} // alternatively, use the `events` setting to fetch from a feed
+            select={this.handleDateSelect}
+            eventContent={renderEventContent} // custom render function
+            eventClick={this.handleEventClick}
+            eventsSet={this.handleEvents} // called after events are initialized/added/changed/removed
+            /* you can update a remote database when these fire:
+            eventAdd={function(){}}
+            eventChange={function(){}}
+            eventRemove={function(){}}
+            */
+            eventAdd={this.saveAppointment}
+          />
+        </div>
+      </div>
+    );
+  }
 
-    setAppointments(records.map((record) => ({
-      title: record.name,
-      date: new Date(record.date),
-    })));
-  })();
+  saveAppointment(appointment) {
+    const newAppointment = {
+      name: appointment.event.title,
+      date: appointment.event.start.toISOString(),
+      user_id: '62c6139c9c28b952c0b65659',
+    };
+    fetch('http://localhost:4000/appointments/new', {
+      method: 'POST',
+      body: JSON.stringify(newAppointment),
+      headers: {
+        'content-type': 'application/json',
+      },
+    });
+  }
 
+  renderSidebar() {
+    return (
+      <div className="demo-app-sidebar">
+        <div className="demo-app-sidebar-section">
+          <h2>Instructions</h2>
+          <ul>
+            <li>Select dates and you will be prompted to create a new event</li>
+            <li>Drag, drop, and resize events</li>
+            <li>Click an event to delete it</li>
+          </ul>
+        </div>
+        <div className="demo-app-sidebar-section">
+          <label>
+            <input
+              type="checkbox"
+              checked={this.state.weekendsVisible}
+              onChange={this.handleWeekendsToggle}
+            />
+            toggle weekends
+          </label>
+        </div>
+        <div className="demo-app-sidebar-section">
+          <h2>
+            All Events (
+            {this.state.currentEvents.length}
+            )
+          </h2>
+          <ul>
+            {this.state.currentEvents.map(renderSidebarEvent)}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+
+  handleWeekendsToggle = () => {
+    this.setState({
+      weekendsVisible: !this.state.weekendsVisible,
+    });
+  };
+
+  handleDateSelect = (selectInfo) => {
+    const title = prompt('Please enter a new title for your event');
+    const calendarApi = selectInfo.view.calendar;
+
+    calendarApi.unselect(); // clear date selection
+
+    if (title) {
+      calendarApi.addEvent({
+        id: createEventId(),
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay,
+      });
+    }
+  };
+
+  handleEventClick = (clickInfo) => {
+    if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
+      clickInfo.event.remove();
+    }
+  };
+
+  handleEvents = (events) => {
+    this.setState({
+      currentEvents: events,
+    });
+  };
+}
+
+function renderEventContent(eventInfo) {
   return (
-    <div>
-      <FullCalendar
-        plugins={[dayGridPlugin]}
-        events={appointments}
-      />
-    </div>
+    <>
+      <b>{eventInfo.timeText}</b>
+      <i>{eventInfo.event.title}</i>
+    </>
   );
 }
 
-export default Calendar;
+function renderSidebarEvent(event) {
+  return (
+    <li key={event.id}>
+      <b>{formatDate(event.start, { year: 'numeric', month: 'short', day: 'numeric' })}</b>
+      <i>{event.title}</i>
+    </li>
+  );
+}
---
+++ client/src/components/event-utils.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable no-use-before-define */
+
+let eventGuid = 0;
+const todayStr = new Date().toISOString().replace(/T.*$/, ''); // YYYY-MM-DD of today
+
+export const INITIAL_EVENTS = [
+  {
+    id: createEventId(),
+    title: 'All-day event',
+    start: todayStr,
+  },
+  {
+    id: createEventId(),
+    title: 'Timed event',
+    start: `${todayStr}T12:00:00`,
+  },
+];
+
+export function createEventId() {
+  return String(eventGuid++);
+}
