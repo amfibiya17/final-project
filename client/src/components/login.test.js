c9853aad480244f1167e4c9d2e89aa8e016c80ed
Anish <anishkakaiya@outlook.com>
2022-07-11 20:41:42 +0100

    add front end tests for login page

--- client/src/components/login.test.js
+++ client/src/components/login.test.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router } from 'react-router-dom';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Login from './login';
 
 test('Email input should be rendered', () => {
@@ -30,4 +30,50 @@ test('Button should be rendered', () => {
   );
   const buttonInputEl = screen.getByRole('button');
   expect(buttonInputEl).toBeInTheDocument();
+});
+
+test('Email input should be empty', () => {
+  render(
+    <Router>
+      <Login />
+    </Router>
+  );
+  const emailInputEl = screen.getByPlaceholderText(/email/i);
+  expect(emailInputEl.value).toBe('');
+});
+
+test('Password input should be empty', () => {
+  render(
+    <Router>
+      <Login />
+    </Router>
+  );
+  const passwordInputEl = screen.getByPlaceholderText(/password/i);
+  expect(passwordInputEl.value).toBe('');
+});
+
+test('Email input should change', () => {
+  render(
+    <Router>
+      <Login />
+    </Router>
+  );
+  const emailInputEl = screen.getByPlaceholderText(/email/i);
+  const testValue = 'test@email.com';
+
+  fireEvent.change(emailInputEl, { target: { value: testValue } });
+  expect(emailInputEl.value).toBe(testValue);
+});
+
+test('Password input should change', () => {
+  render(
+    <Router>
+      <Login />
+    </Router>
+  );
+  const passwordInputEl = screen.getByPlaceholderText(/password/i);
+  const testValue = 'test-password';
+
+  fireEvent.change(passwordInputEl, { target: { value: testValue } });
+  expect(passwordInputEl).toBeInTheDocument();
 });
