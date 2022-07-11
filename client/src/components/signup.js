c531a20e3224a540d74f800bf6af3ba72e7aed48
Yaroslava Yates <yaroslava.yates@gmail.com>
2022-07-11 22:37:34 +0100

    signup-css

--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -1,8 +1,8 @@
+import './signup.css';
+
 import axios from 'axios';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-
-import './signup.css';
 
 function Signup() {
   const navigate = useNavigate();
