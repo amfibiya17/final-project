599c687d783e254658aa1006e95f15c7e39e671b
Yaroslava Yates <yaroslava.yates@gmail.com>
2022-07-14 12:08:05 +0100

    const button added

--- client/src/components/modal.js
+++ client/src/components/modal.js
@@ -8,9 +8,9 @@ const MODAL_STYLES = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  backgroundColor: '#FFF',
   padding: '50px',
   zIndex: 1000,
+  backgroundColor: 'rgba(255, 255, 255, 0.75)',
 };
 
 const OVERLAY_STYLES = {
@@ -23,6 +23,21 @@ const OVERLAY_STYLES = {
   zIndex: 1000,
 };
 
+const BUTTON_STYLES = {
+  backgroundColor: '#00000051',
+  textDecoration: 'none',
+  textAlign: 'center',
+  color: '#00000085',
+  height: '25px',
+  fontWeight: '500',
+  fontFamily: 'inherit',
+  letterSpacing: '0.06em',
+  border: 'none',
+  cursor: 'pointer',
+  marginBottom: '5px',
+  marginTop: '10px',
+};
+
 function Modal({ open, children, onClose }) {
   // const navigate = useNavigate();
   if (!open) return null;
@@ -30,8 +45,10 @@ function Modal({ open, children, onClose }) {
     <>
       <div style={OVERLAY_STYLES} />
       <div style={MODAL_STYLES}>
-        <button type="button" data-cy="close-event-added-modal" onClick={onClose}>Close</button>
-        {children}
+        <div nameClass="modal">
+          {children}
+        </div>
+        <button style={BUTTON_STYLES} type="button" data-cy="close-event-added-modal" onClick={onClose}>Close</button>
       </div>
     </>,
     document.getElementById('portal'),
